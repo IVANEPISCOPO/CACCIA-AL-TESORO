156e173f9a6733aff5f9644a9f61ba11e86237a1
IVANEPISCOPO <162125175+IVANEPISCOPO@users.noreply.github.com>
2024-04-04 09:12:18 +0200

    Update Relazione.js

--- Script/Relazione.js
+++ Script/Relazione.js
@@ -1,6 +1,6 @@
 fuction doLogin (event) { 
  var username = document.getElementById("username").value;
- var username = document.getElementById("username").value;
+ var password = document.getElementById("password").value;
 if(username == "" || password == "") {
    alert( "Username e Password sono obbligatori");
   }
